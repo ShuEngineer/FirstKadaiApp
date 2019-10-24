ab0452e2e4d8fcedb1903aebc1d997b6c0a05f78
ShuEngineer <supercar110shu@gmail.com>
2019-10-24 15:43:45 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by 伊藤嵩 on 2019/10/24.
+//  Copyright © 2019 Shu Ito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
